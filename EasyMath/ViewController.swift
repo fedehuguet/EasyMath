cecdd3f241cb06bd24c5c64a88d2b898e69c7f46
fedehuguet <fede.huguet14@gmail.com>
2017-10-19 12:10:03 -0500

    Initial Commit

---
+++ EasyMath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyMath
+//
+//  Created by Federico Miguel Huguet Mack on 10/19/17.
+//  Copyright © 2017 Federico Miguel Huguet Mack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
