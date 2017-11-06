10d5258613094adfd2c9073d3815fdd5956b6154
fedehuguet <fede.huguet14@gmail.com>
2017-11-06 12:35:41 -0600

    Tableview

--- EasyMath/TableViewController.swift
+++ EasyMath/TableViewController.swift
@@ -12,7 +12,10 @@ class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.backgroundView = UIImageView(image: UIImage(named: "back"))
+        let backgroundImage = UIImage(named: "back")
+        let imageView = UIImageView(image: backgroundImage)
+        self.tableView.backgroundView = imageView
+        //tableView.backgroundView = UIImageView(image: UIImage(named: "back"))
         
     }
 
@@ -27,7 +30,18 @@ class TableViewController: UITableViewController {
         vistaInicial.strLbl = strLbl*/
         
     }
-//    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
-//        cell.backgroundColor = UIColor(white: 1, alpha: 0.5)
-//    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        cell.backgroundColor = UIColor(white: 1, alpha: 0.5)
+        return cell
+    }
 }
