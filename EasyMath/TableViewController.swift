3d2a92b1de176ffb32c096a01d58aec59ddf38d8
fedehuguet <fede.huguet14@gmail.com>
2017-11-06 11:58:27 -0600

    Imagen fondo

--- EasyMath/TableViewController.swift
+++ EasyMath/TableViewController.swift
@@ -12,12 +12,8 @@ class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.backgroundView = UIImageView(image: UIImage(named: "back"))
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,4 +27,7 @@ class TableViewController: UITableViewController {
         vistaInicial.strLbl = strLbl*/
         
     }
+//    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+//        cell.backgroundColor = UIColor(white: 1, alpha: 0.5)
+//    }
 }
