e1265266ffb5aad889a138453e7d0520964d365c
fedehuguet <fede.huguet14@gmail.com>
2017-11-13 14:24:56 -0600

    View controllers para cada celda

--- EasyMath/TableViewController.swift
+++ EasyMath/TableViewController.swift
@@ -16,8 +16,8 @@ class TableViewController: UITableViewController {
         let backgroundImage = UIImage(named: "back")
         let imageView = UIImageView(image: backgroundImage)
         self.tableView.backgroundView = imageView
-        //let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
-        //arrContent = NSArray(contentsOfFile: path!)
+        let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
+        arrContent = NSArray(contentsOfFile: path!)
         
     }
 
@@ -46,17 +46,55 @@ class TableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return arrContent.count
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "polynivel1")
+            self.navigationController?.pushViewController(vc!, animated: true)
+        }
+        else if indexPath.row == 1 {
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "polynivel2")
+            self.navigationController?.pushViewController(vc!, animated: true)
+        }
+        else {
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "trigonivel1")
+            self.navigationController?.pushViewController(vc!, animated: true)
+        }
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! TableViewCell
-        tableViewCell.backgroundColor = UIColor(white: 1, alpha: 0.7)
-        tableViewCell.layer.borderWidth = 2.0
-        tableViewCell.layer.borderColor = UIColor(white: 1, alpha: 0.9).cgColor
-//        let content = (arrContent[indexPath.row] as! NSDictionary)
-//        tableViewCell.lbTitle.text = content["header"] as? String
-//        tableViewCell.lbLevel.text = content["level"] as? String
-//        tableViewCell.imgGraph.image = UIImage(named: (content["graph"] as? String)!)
-        return tableViewCell
+        if indexPath.row == 0 {
+            let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "idCell1", for: indexPath) as! TableViewCell
+            tableViewCell.backgroundColor = UIColor(white: 1, alpha: 0.7)
+            tableViewCell.layer.borderWidth = 2.0
+            tableViewCell.layer.borderColor = UIColor(white: 1, alpha: 0.9).cgColor
+            let content = (arrContent[indexPath.row] as! NSDictionary)
+            tableViewCell.lbTitle.text = content["header"] as? String
+            tableViewCell.lbLevel.text = content["level"] as? String
+            tableViewCell.imgGraph.image = UIImage(named: (content["graph"] as? String)!)
+            return tableViewCell
+        }
+        else if indexPath.row == 1 {
+            let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "idCell2", for: indexPath) as! TableViewCell
+            tableViewCell.backgroundColor = UIColor(white: 1, alpha: 0.7)
+            tableViewCell.layer.borderWidth = 2.0
+            tableViewCell.layer.borderColor = UIColor(white: 1, alpha: 0.9).cgColor
+            let content = (arrContent[indexPath.row] as! NSDictionary)
+            tableViewCell.lbTitle.text = content["header"] as? String
+            tableViewCell.lbLevel.text = content["level"] as? String
+            tableViewCell.imgGraph.image = UIImage(named: (content["graph"] as? String)!)
+            return tableViewCell
+        }
+        else {
+            let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "idCell3", for: indexPath) as! TableViewCell
+            tableViewCell.backgroundColor = UIColor(white: 1, alpha: 0.7)
+            tableViewCell.layer.borderWidth = 2.0
+            tableViewCell.layer.borderColor = UIColor(white: 1, alpha: 0.9).cgColor
+            let content = (arrContent[indexPath.row] as! NSDictionary)
+            tableViewCell.lbTitle.text = content["header"] as? String
+            tableViewCell.lbLevel.text = content["level"] as? String
+            tableViewCell.imgGraph.image = UIImage(named: (content["graph"] as? String)!)
+            return tableViewCell
+        }
     }
 }
---
+++ EasyMath/ViewControllerTrigoN1.swift
@@ -0,0 +1,35 @@
+//
+//  ViewControllerTrigoN1.swift
+//  EasyMath
+//
+//  Created by Federico Miguel Huguet Mack on 11/13/17.
+//  Copyright © 2017 Federico Miguel Huguet Mack. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTrigoN1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
