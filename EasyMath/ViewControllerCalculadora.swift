d1f2aace8b47935a9fb7a887f8ea6343d64360bb
atamez31 <andres.96hdz@hotmail.com>
2017-11-13 19:30:55 -0600

    Diseño Calculadora

--- EasyMath/ViewControllerCalculadora.swift
+++ EasyMath/ViewControllerCalculadora.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewControllerCalculadora: UIViewController {
 
+    @IBOutlet weak var btnCuadrada: UIButton!
+    @IBOutlet weak var btnPoli: UIButton!
+    @IBOutlet weak var btnSeno: UIButton!
+    @IBOutlet weak var btnCoseno: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let borderAlpha : CGFloat = 0.7
+        btnSeno.layer.borderWidth = 2.0
+        btnSeno.layer.borderColor = UIColor(white: 2.0, alpha: borderAlpha).cgColor
+        
+        btnPoli.layer.borderWidth = 2.0
+        btnPoli.layer.borderColor = UIColor(white: 2.0, alpha: borderAlpha).cgColor
+        
+        btnCuadrada.layer.borderWidth = 2.0
+        btnCuadrada.layer.borderColor = UIColor(white: 2.0, alpha: borderAlpha).cgColor
+        
+        btnCoseno.layer.borderWidth = 2.0
+        btnCoseno.layer.borderColor = UIColor(white: 2.0, alpha: borderAlpha).cgColor
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +37,21 @@ class ViewControllerCalculadora: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    
+    //Funciones de derivadas
+    func derivativeOf(fn: (Double)->Double, atX x: Double) -> Double {
+        let h = 0.0000001
+        return (fn(x + h) - fn(x))/h
     }
-    */
+    
+    func x_squared(x: Double) -> Double {
+        return x * x
+    }
+    
+    func makeThirdOrderPolynomial(A a: Double, B b: Double, C c: Double, D d: Double) -> ((Double) -> Double) {
+        return { x in ((a * x + b) * x + c) * x + d }
+    }
 
 }
